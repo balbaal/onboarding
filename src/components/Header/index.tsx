ffe6dc60851f0d250689350d8cdeff3a8ef2f8b9
Iqbal <syfr.iqbal@gmail.com>
2025-07-27 10:31:30 +0700

    feat: hover animate brand character header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,21 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
 import { StepNumber, SwipeIndex, useOnboardingStep } from "@/store/useOnboardingStep";
 import { useSwiper } from "@/store/useSwiper";
 import styles from "./style.module.css";
 import ButtonControl from "../ButtonControl";
 import ArrowLeftIcon from "@/assets/icons/arrow-left.svg";
 import RefreshIcon from "@/assets/icons/refresh.svg";
+
+import J from "@/assets/images/brand-character/j.svg";
+import U from "@/assets/images/brand-character/u.svg";
+import I from "@/assets/images/brand-character/I.svg";
+import C from "@/assets/images/brand-character/C.svg";
+import E from "@/assets/images/brand-character/E.svg";
+import B from "@/assets/images/brand-character/B.svg";
+import O from "@/assets/images/brand-character/O.svg";
+import X from "@/assets/images/brand-character/X.svg";
 
 const Header = () => {
   const { setOnboardingStep, setSwipeIndex, swipeIndex, onboardingStep } = useOnboardingStep();
@@ -39,12 +47,16 @@ const Header = () => {
           icon={<ArrowLeftIcon width={20} height={20} />}
         />
       </div>
-      <Image
-        src="/juicebox-logo-white.svg"
-        alt="juice box brand logo white"
-        width={123}
-        height={29}
-      />
+      <div className={styles.BrandWrap}>
+        <J className={styles.Char1} />
+        <U className={styles.Char2} />
+        <I className={styles.Char3} />
+        <C className={styles.Char4} />
+        <E className={styles.Char5} />
+        <B className={styles.Char6} />
+        <O className={styles.Char7} />
+        <X className={styles.Char8} />
+      </div>
       <ButtonControl
         ariaLabel="refresh button for go back to first onboarding"
         onClick={handleClickRefresh}
